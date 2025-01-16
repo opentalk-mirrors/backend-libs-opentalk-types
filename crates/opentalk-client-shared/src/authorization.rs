78effec1accfd8fb55f51973bb9208d9fcea6bed
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-01-16 16:08:50 +0100

    feat: remove opentalk-client-shared from this repository
    Closes #7

--- crates/opentalk-client-shared/src/authorization.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
-//
-// SPDX-License-Identifier: EUPL-1.2
-
-/// Trait for augmenting requests with authentication information
-pub trait Authorization: std::fmt::Debug {
-    /// Add authorization headers
-    fn apply_authorization_headers(&self, headers: &mut http::HeaderMap);
-}
