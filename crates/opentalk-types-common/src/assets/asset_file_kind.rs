f8ef5e06d7db29f1f9653bf382ef75b48e1a5f81
Werner Rabl <werner.rabl@opentalk.eu>
2025-01-27 10:24:23 +0100

    feat: add types for creating assets - like PostAssetQuery, PostAssetResponseBody, AssetFileKind

---
+++ crates/opentalk-types-common/src/assets/asset_file_kind.rs
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+use std::str::FromStr;
+
+use snafu::Snafu;
+
+use crate::utils::ExampleData;
+
+/// The minimum length for a file kind
+pub const MIN_ASSET_FILE_KIND_LENGTH: usize = 1;
+/// The maximum length for a file kind
+pub const MAX_ASSET_FILE_KIND_LENGTH: usize = 20;
+
+/// The kind of asset
+///
+/// Can be parsed using [`std::str::FromStr`].
+/// May contain alphanumeric ascii characters and underscores only, and is restricted to a
+/// length of [`MIN_ASSET_FILE_KIND_LENGTH`] up to [`MAX_ASSET_FILE_KIND_LENGTH`] characters.
+/// This serves as sanitization measure for user input.
+#[derive(Debug, Clone, Default, PartialEq, Eq, derive_more::Display)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde_with::DeserializeFromStr)
+)]
+pub struct AssetFileKind(String);
+
+#[derive(Debug, Snafu)]
+pub enum ParseAssetFileKindError {
+    #[snafu(display("AssetFileKind must be at least {min_length} characters long"))]
+    TooShort { min_length: usize },
+
+    #[snafu(display("AssetFileKind must not be longer than {max_length} characters"))]
+    TooLong { max_length: usize },
+
+    #[snafu(display("AssetFileKind only allows alphanumeric ascii characters or '_'"))]
+    InvalidCharacters,
+}
+
+impl FromStr for AssetFileKind {
+    type Err = ParseAssetFileKindError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < MIN_ASSET_FILE_KIND_LENGTH {
+            return Err(ParseAssetFileKindError::TooShort {
+                min_length: MIN_ASSET_FILE_KIND_LENGTH,
+            });
+        }
+        if s.len() > MAX_ASSET_FILE_KIND_LENGTH {
+            return Err(ParseAssetFileKindError::TooLong {
+                max_length: MAX_ASSET_FILE_KIND_LENGTH,
+            });
+        }
+        if s.chars().any(|c| !(c.is_ascii_alphanumeric() || c == '_')) {
+            return Err(ParseAssetFileKindError::InvalidCharacters);
+        }
+        Ok(AssetFileKind(s.into()))
+    }
+}
+
+#[cfg(feature = "utoipa")]
+mod impl_utoipa {
+    use serde_json::json;
+    use utoipa::{
+        openapi::{ObjectBuilder, RefOr, Schema, Type},
+        PartialSchema, ToSchema,
+    };
+
+    use super::{AssetFileKind, MAX_ASSET_FILE_KIND_LENGTH, MIN_ASSET_FILE_KIND_LENGTH};
+    use crate::utils::ExampleData;
+
+    impl PartialSchema for AssetFileKind {
+        fn schema() -> RefOr<Schema> {
+            ObjectBuilder::new()
+                .schema_type(Type::String)
+                .min_length(Some(MIN_ASSET_FILE_KIND_LENGTH))
+                .max_length(Some(MAX_ASSET_FILE_KIND_LENGTH))
+                .pattern(Some("^[0-9a-zA-Z_]*$".to_string()))
+                .description(Some("An asset file kind"))
+                .examples([json!(AssetFileKind::example_data())])
+                .into()
+        }
+    }
+
+    impl ToSchema for AssetFileKind {
+        fn schemas(schemas: &mut Vec<(String, RefOr<Schema>)>) {
+            schemas.push((Self::name().into(), Self::schema()));
+        }
+    }
+}
+
+impl ExampleData for AssetFileKind {
+    fn example_data() -> Self {
+        Self("mykind".to_string())
+    }
+}
--- crates/opentalk-types-common/src/assets/mod.rs
+++ crates/opentalk-types-common/src/assets/mod.rs
@@ -4,10 +4,12 @@
 
 //! Data types for handling assets.
 
+mod asset_file_kind;
 mod asset_id;
 mod asset_sorting;
 mod file_extension;
 
+pub use asset_file_kind::{AssetFileKind, MAX_ASSET_FILE_KIND_LENGTH, MIN_ASSET_FILE_KIND_LENGTH};
 pub use asset_id::AssetId;
 pub use asset_sorting::AssetSorting;
 pub use file_extension::{FileExtension, MAX_FILE_EXTENSION_LENGTH};
