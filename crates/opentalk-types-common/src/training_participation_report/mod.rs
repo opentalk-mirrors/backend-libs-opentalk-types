4460652b27074f0b4a4237523649988aaa30ddb9
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-03-07 11:15:33 +0100

    feat: add TrainingParticipationReportParameterSet
    See #16

--- crates/opentalk-types-common/src/training_participation_report/mod.rs
+++ crates/opentalk-types-common/src/training_participation_report/mod.rs
@@ -5,5 +5,7 @@
 //! Data types for handling training participation reports.
 
 mod time_range;
+mod training_participation_report_parameter_set;
 
 pub use time_range::TimeRange;
+pub use training_participation_report_parameter_set::TrainingParticipationReportParameterSet;
