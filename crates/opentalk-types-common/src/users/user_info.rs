8d07d4c7fb679267db613e63b91c1a3eeca2540b
Sandro Forster <s.forster@opentalk.eu>
2025-03-18 17:42:47 +0100

    refactor: rename CreatorInfo to UserInfo and move it to opentalk-types-common

--- crates/opentalk-types-common/src/users/user_info.rs
+++ crates/opentalk-types-common/src/users/user_info.rs
@@ -2,17 +2,25 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-use opentalk_types_common::users::{DisplayName, UserTitle};
+use crate::{
+    users::{DisplayName, UserTitle},
+    utils::ExampleData,
+};
 
 /// Information about the room creator
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema), schema(
+    example = json!(
+        UserInfo::example_data()
+    )
+))]
 #[cfg_attr(
     feature = "redis",
     derive(redis_args::ToRedisArgs, redis_args::FromRedisValue)
 )]
 #[cfg_attr(feature = "redis", to_redis_args(serde), from_redis_value(serde))]
-pub struct CreatorInfo {
+pub struct UserInfo {
     /// Optional title of the creator
     pub title: UserTitle,
 
@@ -27,4 +35,16 @@ pub struct CreatorInfo {
 
     /// The creators avatar url
     pub avatar_url: String,
+}
+
+impl ExampleData for UserInfo {
+    fn example_data() -> Self {
+        UserInfo {
+            title: "".parse().expect("valid user title"),
+            firstname: "Alice".to_string(),
+            lastname: "Adams".to_string(),
+            display_name: "Alice Adams".parse().expect("valid display name"),
+            avatar_url: "https://gravatar.com/avatar/c160f8cc69a4f0bf2b0362752353d060".to_string(),
+        }
+    }
 }
