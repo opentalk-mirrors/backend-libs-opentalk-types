c6f7ba7e62b45d325b7a229e2bec57a8b502f72f
Sandro Forster <s.forster@opentalk.eu>
2026-03-04 17:03:56 +0100

    feat(api-v1)!: use total and used quota in `PostAssetResponseBody` instead of remaining quota

--- crates/opentalk-types-api-v1/src/services/roomserver/post_asset_response_body.rs
+++ crates/opentalk-types-api-v1/src/services/roomserver/post_asset_response_body.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: EUPL-1.2
 // SPDX-FileCopyrightText: OpenTalk Team <mail@opentalk.eu>
 
-use crate::assets::AssetResource;
+use crate::assets::{AssetResource, Quota};
 
 /// Response for the `POST /services/roomserver/room/{room_id}/asset` endpoint
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -11,6 +11,6 @@ pub struct PostAssetResponseBody {
     /// The asset that was uploaded
     pub asset_resource: AssetResource,
 
-    /// The remaining quota in bytes for the room, if applicable.
-    pub remaining_quota_bytes: Option<u64>,
+    /// The total and used quota in bytes
+    pub quota: Quota,
 }
