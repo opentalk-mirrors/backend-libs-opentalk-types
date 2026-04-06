7070aa8ca6a28ff156dc2f0eb71bbeaa19ae4c5f
Oleksii Sukhodolskyi <o.sukhodolskyi@opentalk.eu>
2026-04-06 20:11:45 +0200

    feat(room): add guest access mode
    Closes #56

--- crates/opentalk-types-common/src/rooms/mod.rs
+++ crates/opentalk-types-common/src/rooms/mod.rs
@@ -7,9 +7,11 @@
 pub mod invite_codes;
 
 mod breakout_room_id;
+mod guest_access;
 mod room_id;
 mod room_password;
 
 pub use breakout_room_id::BreakoutRoomId;
+pub use guest_access::{GuestAccess, GuestAccessType};
 pub use room_id::RoomId;
 pub use room_password::{ROOM_PASSWORD_MAX_LENGTH, ROOM_PASSWORD_MIN_LENGTH, RoomPassword};
